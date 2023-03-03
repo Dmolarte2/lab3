8bd6fd0e29a8da5a065f412eec1eab2149478d7d
dlarte <dmolarte2@student.apc.edu.ph>
2023-03-03 17:50:11 +0800

    final webprog7

--- ci4/app/Views/pages/guests.php
+++ ci4/app/Views/pages/guests.php
@@ -1,26 +1,45 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "testDB";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-} 
+<!DOCTYPE html>
+<html>
+<body>
 
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
-$result = $conn->query($sql);
+<div class="sidenav">
+  <a href="index.php">Go Back to My Profile</a>
+</div>
 
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-  }
-} else {
-  echo "0 results";
-}
-$conn->close();
-?>
+<div class="content">
+  <h2>Applicants for Daniel's Dogs</h2>
+  <p>
+  <?php
+          $servername = "192.168.150.213";
+          $username = "webprogmi212";
+          $password = "b3ntRhino98";
+          $dbname = "webprogmi212";
+          
+          // Create connection
+          $conn = new mysqli($servername, $username, $password, $dbname);
+          // Check connection
+          if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+          }
+
+          $sql = "SELECT name, email, website, comment, gender FROM dmolarte2_MyGuestss";
+          $result = $conn->query($sql);
+
+          if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+              echo "<br> Name: " . $row["name"]. " - Email: " . $row["email"]. " - Website: " . $row["website"].
+              " - Comment: " . $row["comment"]. " - Gender: " . $row["gender"]. "<br>";
+            }
+          } else {
+            echo "0 results";
+          }
+          $conn->close();  
+  ?>
+</p>
+</div>
+
+
+</body>
+</html>
--- ci4/app/Views/pages/index.php
+++ ci4/app/Views/pages/index.php
@@ -152,12 +152,9 @@ PI = PI + 10;   // This will also give an error
 		<p><a href="http://apcwebprog.csf.ph/~dmolarte2/lab3/ci4/public/news"><i class="fa fa-book w3-hover-opacity w3-margin-right"></i> </a> <a href="http://apcwebprog.csf.ph/~dmolarte2/lab3/ci4/public/news">View News Archive</a></p>
 		
     
-    </div><br>
-    
+    </div><br> 
 
   </div>
-
-
 
     <div class=" w3-content" id="contact">
     <h2 style="font-color:#0F0F0F;">Contact Me</h2>
@@ -173,6 +170,130 @@ PI = PI + 10;   // This will also give an error
     
 
   </div>
+
+
+  <?php
+    // define variables and set to empty values
+    $nameErr = $emailErr = $genderErr = $websiteErr = "";
+    $name = $email = $gender = $comment = $website = "";
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      if (empty($_POST["name"])) {
+        $nameErr = "Name is required";
+      } else {
+        $name = test_input($_POST["name"]);
+        // check if name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+          $nameErr = "Only letters and white space allowed";
+        }
+      }
+      
+      if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+      } else {
+        $email = test_input($_POST["email"]);
+        // check if e-mail address is well-formed
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+          $emailErr = "Invalid email format";
+        }
+      }
+        
+      if (empty($_POST["website"])) {
+        $website = "";
+      } else {
+        $website = test_input($_POST["website"]);
+        // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+        if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+          $websiteErr = "Invalid URL";
+        }
+      }
+    
+      if (empty($_POST["comment"])) {
+        $comment = "";
+      } else {
+        $comment = test_input($_POST["comment"]);
+      }
+    
+      if (empty($_POST["gender"])) {
+        $genderErr = "Gender is required";
+      } else {
+        $gender = test_input($_POST["gender"]);
+      }
+    }
+    
+    function test_input($data) {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+    }
+    ?>
+    
+    <h2>Application for Daniel's Dogs</h2>
+    <hr style="height:2px;border-width:0;width:60%px;color:gray;background-color:#0F0F0F">
+    <p><span class="error">* required field</span></p>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+      Name: <input type="text" name="name" value="<?php echo $name;?>">
+      <span class="error">* <?php echo $nameErr;?></span>
+      <br><br>
+      E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+      <span class="error">* <?php echo $emailErr;?></span>
+      <br><br>
+      Website: <input type="text" name="website" value="<?php echo $website;?>">
+      <span class="error"><?php echo $websiteErr;?></span>
+      <br><br>
+      Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+      <br><br>
+      Gender:
+      <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+      <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+      <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
+      <span class="error">* <?php echo $genderErr;?></span>
+      <br><br>
+      <input type="submit" name="submit" value="Submit">  
+    </form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
+     
+    <?php
+      if ($_SERVER["REQUEST_METHOD"] == "POST") 
+      {
+        $servername = "192.168.150.213";
+        $username = "webprogmi212";
+        $password = "b3ntRhino98";
+        $dbname = "webprogmi212";
+        
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+        }
+        
+        $sql = "INSERT INTO dmolarte2_MyGuestss (name, email, website, comment, gender)
+        VALUES ('$name', '$email', '$website', '$comment', '$gender')";
+        
+        if ($conn->query($sql) === TRUE) {
+          echo "New record created successfully";
+        } else {
+          echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+        
+        $conn->close();
+      }
+  
+    ?>        
      
     <!-- baba -->
   <footer class="w3-content w3-padding-64 w3-xlarge">
